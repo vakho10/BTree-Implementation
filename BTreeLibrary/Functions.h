a566bd5d733f19b9a72ddeeb71ac54a476873109
CRA\v.laluashvili <vakho10@gmail.com>
2017-03-21 14:47:26 +0400

    Added more comparators.

--- BTreeLibrary/Functions.h
+++ BTreeLibrary/Functions.h
@@ -24,7 +24,7 @@ template<typename T, typename Extract = PlaintKeyExtractor<T>>
 struct Less : std::binary_function<T, T, bool>
 {
 	Extract ext;
-	Less(Extract& _ext = Extract()) 
+	Less(Extract& _ext = Extract())
 	{
 		ext = _ext;
 	}
@@ -32,5 +32,53 @@ struct Less : std::binary_function<T, T, bool>
 	result_type operator()(first_argument_type& firstArg, second_argument_type& secondArg)
 	{
 		return ext(firstArg) < ext(secondArg);
+	}
+};
+
+// LessOrEqual comparator
+template<typename T, typename Extract = PlaintKeyExtractor<T>>
+struct LessOrEqual : std::binary_function<T, T, bool>
+{
+	Extract ext;
+	Less(Extract& _ext = Extract())
+	{
+		ext = _ext;
+	}
+
+	result_type operator()(first_argument_type& firstArg, second_argument_type& secondArg)
+	{
+		return ext(firstArg) <= ext(secondArg);
+	}
+};
+
+// More comparator
+template<typename T, typename Extract = PlaintKeyExtractor<T>>
+struct More : std::binary_function<T, T, bool>
+{
+	Extract ext;
+	Less(Extract& _ext = Extract())
+	{
+		ext = _ext;
+	}
+
+	result_type operator()(first_argument_type& firstArg, second_argument_type& secondArg)
+	{
+		return ext(firstArg) > ext(secondArg);
+	}
+};
+
+// MoreOrEqual comparator
+template<typename T, typename Extract = PlaintKeyExtractor<T>>
+struct MoreOrEqual : std::binary_function<T, T, bool>
+{
+	Extract ext;
+	Less(Extract& _ext = Extract())
+	{
+		ext = _ext;
+	}
+
+	result_type operator()(first_argument_type& firstArg, second_argument_type& secondArg)
+	{
+		return ext(firstArg) >= ext(secondArg);
 	}
 };
